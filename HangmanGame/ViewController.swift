b124871efda095de725c613710dd910c20692af1
SUP'Internet 08 <supinternet08@macbook-air-de-supinternet-41.local>
2018-11-20 14:24:49 +0100

    Initial Commit

---
+++ HangmanGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HangmanGame
+//
+//  Created by SUP'Internet 08 on 20/11/2018.
+//  Copyright © 2018 SUP'Internet 08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
