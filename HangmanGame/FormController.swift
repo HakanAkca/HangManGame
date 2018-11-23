6cdd3e0cb53d1932a8529f7f93fa48f1aa2d6952
SUP'Internet 04 <supinternet04@MacBook-Air-de-SUPInternet-54.local>
2018-11-23 14:33:26 +0100

    new version

---
+++ HangmanGame/FormController.swift
@@ -0,0 +1,68 @@
+//
+//  FormController.swift
+//  HangmanGame
+//
+//  Created by SUP'Internet 08 on 20/11/2018.
+//  Copyright © 2018 SUP'Internet 08. All rights reserved.
+//
+
+import UIKit
+
+class FormController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pw: UITextField!
+    
+    
+    @IBAction func btnForm(_ sender: Any) {
+        let url = URL(string: "http://edu2.shareyourtime.fr/api/auth")!
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let postString = "email=sahra.vadrot@supinternet.fr&password=SUP00000"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            //let responseString = String(data: data, encoding: .utf8)
+            //print("responseString = \(responseString)")
+            do{
+                //here dataResponse received from a network request
+                let jsonResponse = try JSONSerialization.jsonObject(with:
+                    data, options: [])
+                print(jsonResponse) //Response result
+            } catch let parsingError {
+                print("Error", parsingError)
+            }
+        }
+        task.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
